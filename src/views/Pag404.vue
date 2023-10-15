1dda2c1900bc38b0ae942d117f35caf4ffa17d64
Papuchi <johndoe@example.com>
2023-10-15 19:30:29 +0200

    cmabiando la 404

--- src/views/Pag404.vue
+++ src/views/Pag404.vue
@@ -8,7 +8,7 @@
       <center class="pt-52">
         <h1 class="text-orange-300 text-6xl font-black">404</h1>
         <h2 class="text-white text-3xl mt-10">Qué vacío está todo</h2>
-        <h3 class="text-white text-2xl mt-10">Tienes los deditos como morcillas</h3>
+        <h3 class="text-white text-2xl mt-10">Tienes los deditos como morcillas de arroz</h3>
       </center>
    </div>
 </template>
